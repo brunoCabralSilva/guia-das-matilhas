2d7cffa16153cbe23e22c588a07c782f59549fe5
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-17 10:35:21 -0300

    corrigindo fron

--- front-end/src/components/Ficha/Experiencia.jsx
+++
@@ -1,8 +0,0 @@
-export default function Experiencia() {
-  return (
-    <div className="flex flex-col relative z-30">
-      <p className="py-4 text-2xl font-bold">Experiência</p>
-      <input type="number" className="h-16 p-2" />
-    </div>
-  );
-}
--- front-end/src/fetch.jsx
+++
@@ -1,5 +0,0 @@
-export default function fetch() {
-  const HOST = process.env.REACT_APP_API_HOST || 'localhost:3301';
-  const PROTOCOL = process.env.REACT_APP_PROTOCOL || 'http';
-  return (`${PROTOCOL}://${HOST}`);
-}
