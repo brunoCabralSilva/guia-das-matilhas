2ec3d56cdc85e9b84a411baf84442c4750111cdb
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-17 14:46:18 -0300

    Menu

--- front-end/src/components/Transition.jsx
+++ front-end/src/components/Transition.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 export default class Transition extends React.Component {
   render() {
+    const { homeReturn } = this.props;
     return (
       <section className="flex flex-row relative justify-between items-center w-full h-full bg-w20 bg-center">
         <div className="absolute w-full h-full bg-7-transp" />
-          {/* <img
+          <img
             src={require('../images/logos/arrow-left.png')}
             alt="Seta para baixo"
             className="h-50% sm:h-80% w-24 mx-1 sm:mx-4 z-40 animate-pulse hover:animate-pulse-fast object-contain"
             onClick={homeReturn}
-          /> */}
+          />
         <div className="flex flex-col items-end justify-center px-4 font-andika text-right text-xl">
           <h3 className="z-20 sm:text-3xl font-bold">
             "Que Gaia tenha piedade de nós!"
