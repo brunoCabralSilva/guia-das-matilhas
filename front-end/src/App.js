fd5acfe70798cc55b28982c611c067e98ab69e04
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-17 10:52:57 -0300

    removendo itens para ver se o container é criado sem eles

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,29 @@
-import { Switch, Route } from 'react-router-dom';
+// import { Switch, Route } from 'react-router-dom';
 import { AnimateSharedLayout, AnimatePresence } from 'framer-motion';
 import './App.css';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Menu from './pages/Menu';
-import MatilhaDaKombi from './pages/MatilhaDaKombi';
-import GarouNordeste from './pages/GarouNordeste';
-import Page from './pages/Page';
-import Rituals from './pages/Rituals';
-import Friends from './pages/Friends';
-import AboutUs from './pages/AboutUs';
-import Trybes from './pages/Trybes';
-import Breeds from './pages/Breeds';
-import Auspices from './pages/Auspices';
-import Gifts from './pages/Gifts';
-import Login from './pages/Login';
-import Painel from './pages/admin/Painel';
-import RegisterGift from './pages/admin/RegisterGift';
-import Ficha from './components/Ficha/Ficha';
+// import Home from './pages/Home';
+// import NotFound from './pages/NotFound';
+// import Menu from './pages/Menu';
+// import MatilhaDaKombi from './pages/MatilhaDaKombi';
+// import GarouNordeste from './pages/GarouNordeste';
+// import Page from './pages/Page';
+// import Rituals from './pages/Rituals';
+// import Friends from './pages/Friends';
+// import AboutUs from './pages/AboutUs';
+// import Trybes from './pages/Trybes';
+// import Breeds from './pages/Breeds';
+// import Auspices from './pages/Auspices';
+// import Gifts from './pages/Gifts';
+// import Login from './pages/Login';
+// import Painel from './pages/admin/Painel';
+// import RegisterGift from './pages/admin/RegisterGift';
+// import Ficha from './components/Ficha/Ficha';
 
 function App() {
   return (
     <AnimateSharedLayout>
       <AnimatePresence>
-        <Switch>
+        {/* <Switch>
           <Route path='/login' component={Login} />
           <Route path="/painel-admin" component={Painel} />
           <Route path="/painel-dons" component={RegisterGift} />
@@ -41,7 +41,7 @@ function App() {
           <Route path="/rituais" component={Rituals} />
           <Route path="/parceiros" component={Friends} />
           <Route exact path="*" component={NotFound} />
-        </Switch>
+        </Switch> */}
       </AnimatePresence>
     </AnimateSharedLayout>
   );
--- front-end/src/components/Ficha/Desvantagens.jsx
+++
@@ -1,8 +0,0 @@
-export default function Desvantagens() {
-  return (
-    <div className="flex flex-col relative z-30">
-      <p className="py-4 text-2xl font-bold">Desvantagens</p>
-      <textarea className="h-24" />
-    </div>
-  );
-}
