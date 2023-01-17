454d2ed59a57f86d01a05798126c1dabe933c354
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-16 22:17:07 -0300

    teste de conexão do back-end

--- back-end/src/getAll.js
+++ back-end/src/getAll.js
@@ -2,6 +2,7 @@ const connection = require('./api/connection');
 
 const getAll = async () => {
     const [get] = await connection.execute('SELECT * FROM trybes');
+    return get;
 };
 
 module.exports = getAll;
