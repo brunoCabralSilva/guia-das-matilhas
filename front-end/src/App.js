ad69d676f4cbb8b967da64b05db32ad4b3b4bd04
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-17 11:35:29 -0300

    APenas pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,18 +2,18 @@ import { Switch, Route } from 'react-router-dom';
 import { AnimateSharedLayout, AnimatePresence } from 'framer-motion';
 import './App.css';
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Menu from './pages/Menu';
-import MatilhaDaKombi from './pages/MatilhaDaKombi';
-import GarouNordeste from './pages/GarouNordeste';
-import Page from './pages/Page';
-import Rituals from './pages/Rituals';
-import Friends from './pages/Friends';
-import AboutUs from './pages/AboutUs';
-import Trybes from './pages/Trybes';
-import Breeds from './pages/Breeds';
-import Auspices from './pages/Auspices';
-import Gifts from './pages/Gifts';
+// import NotFound from './pages/NotFound';
+// import Menu from './pages/Menu';
+// import MatilhaDaKombi from './pages/MatilhaDaKombi';
+// import GarouNordeste from './pages/GarouNordeste';
+// import Page from './pages/Page';
+// import Rituals from './pages/Rituals';
+// import Friends from './pages/Friends';
+// import AboutUs from './pages/AboutUs';
+// import Trybes from './pages/Trybes';
+// import Breeds from './pages/Breeds';
+// import Auspices from './pages/Auspices';
+// import Gifts from './pages/Gifts';
 // import Login from './pages/Login';
 // import Painel from './pages/admin/Painel';
 // import RegisterGift from './pages/admin/RegisterGift';
@@ -30,7 +30,7 @@ function App() {
           <Route path="/painel-dons" component={RegisterGift} /> */}
           {/* <Route path="/ficha" component={Ficha} /> */}
           <Route exact path="/guia-das-matilhas" component={Home} />
-          <Route path="/trybes" component={Trybes} />
+          {/* <Route path="/trybes" component={Trybes} />
           <Route path="/breeds" component={Breeds} />
           <Route path="/auspices" component={Auspices} />
           <Route path="/about" component={AboutUs} />
@@ -41,7 +41,7 @@ function App() {
           <Route path="/page" component={Page} />
           <Route path="/rituais" component={Rituals} />
           <Route path="/parceiros" component={Friends} />
-          <Route exact path="*" component={NotFound} />
+          <Route exact path="*" component={NotFound} /> */}
         </Switch>
       </AnimatePresence>
     </AnimateSharedLayout>
