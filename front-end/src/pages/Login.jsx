27fdebe97e8c6e0989de7b60ee40887d965cebe6
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-18 20:59:21 -0300

    incluindo tela de loading

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -10,16 +10,20 @@ export default class Login extends React.Component{
   };
   
   async componentDidMount() {
+    console.log('entrou');
     this.setState({ loading: true });
     const { history } = this.props;
     try {
       const token = localStorage.getItem('token');
       if (token) {
-      const resp =  await axios.post(`${fetch()}/login/verify`, { token });
+        const resp = await axios.post(`${fetch()}/login/verify`, { token });
+        this.setState({ loading: false });
+        if(resp) {
+          history.push('/painel-admin');
+        }
+        this.setState({ loading: false });
+      } 
       this.setState({ loading: false });
-      if(resp) {
-        history.push('/painel-admin');
-      }}
     } catch(error) {
       this.setState({ loading: false });
     }
@@ -57,27 +61,33 @@ export default class Login extends React.Component{
     }
     return (
       <div className="w-screen h-screen flex items-center justify-center">
-        <form onSubmit={this.submit} className="flex flex-col">
-          <input
-            type="text"
-            placeholder="email"
-            className="p-2 m-2 text-center"
-            onChange={(e) => this.setState({ user: e.target.value })}
-          />
-          <input
-            type="password"
-            placeholder="password"
-            className="p-2 m-2 text-center"
-            onChange={(e) => this.setState({ password: e.target.value })}
-          />
-          <button
-            type="button"
-            onClick={this.submit}
-            className="p-2 m-2 bg-black text-white"
-          >
-            Entrar
-          </button>
-        </form>
+        {
+          loading ?
+          <div className="h-screen w-full flex items-center justify-center">
+            <span className="loader"></span>
+          </div>
+          : <form onSubmit={this.submit} className="flex flex-col">
+            <input
+              type="text"
+              placeholder="email"
+              className="p-2 m-2 text-center"
+              onChange={(e) => this.setState({ user: e.target.value })}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              className="p-2 m-2 text-center"
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
+            <button
+              type="button"
+              onClick={this.submit}
+              className="p-2 m-2 bg-black text-white"
+            >
+              Entrar
+            </button>
+            </form>
+        }
       </div>
     );
   }
