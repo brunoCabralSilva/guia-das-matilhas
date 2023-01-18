8718a488676584a99680d677403b145689652706
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-17 22:06:29 -0300

    corrigindo link de direcionamento e imagens de menu

--- front-end/src/fetch.js
+++ front-end/src/fetch.js
@@ -1,5 +1,5 @@
 export default function fetch() {
-  const HOST = process.env.REACT_APP_API_HOST || 'https://guia-das-matilhas-back-end.up.railway.app/';
+  const HOST = process.env.REACT_APP_API_HOST || 'https://guia-das-matilhas-back-end.up.railway.app';
   const PROTOCOL = process.env.REACT_APP_PROTOCOL || 'https';
   return (`${PROTOCOL}://${HOST}`);
 }
--- front-end/src/pages/admin/Painel.jsx
+++ front-end/src/pages/admin/Painel.jsx
@@ -5,7 +5,7 @@ import fetch from '../../fetch';
 
 const listMenu = [
   {
-    img: 'https://64.media.tumblr.com/211b80132af34bd972de8120549c5bd8/68794e779f4c07af-b6/s540x810/9bf323293d4da6cd7bea260ca02a3fdfeefed162.gif',
+    img: 'https://64.media.tumblr.com/e3a999e989e0f17dac10be8b649707eb/af64a96270ae26f2-dd/s400x600/c1164ced59af1891c7c60ca70613a4d4643d0bb9.gifv',
     name: 'Tribos',
     col: 'col-span-3 sm:col-span-2',
   },
@@ -15,38 +15,38 @@ const listMenu = [
     col: 'col-span-2 sm:col-span-2',
   },
   {
-    img: 'https://64.media.tumblr.com/1d963ed7421196b4d274b4fdf372fb57/9dfb927208d4ab3b-fc/s540x810/6b24aea8c33dfc0529c924af358abe69b5e4043b.gifv',
+    img: 'https://media4.giphy.com/media/S9hmjJG3ZL4gCSWXze/giphy.gif?cid=790b7611a6244ac30bd8cab58718cf6f40efb92a3b78e81a&rid=giphy.gif&ct=g',
     name: 'Augúrios',
     col: 'col-span-2 sm:col-span-1',
   },
   {
-    img: 'https://www.wikitree.com/photo.php/f/f9/Cook-24870-1.gif',
+    img: 'https://64.media.tumblr.com/4fdd580543a70f91cca8ad8c3536b13a/68794e779f4c07af-37/s540x810/d10a99c1fba35bb8ae9416029915ddfd8d3ab675.gif',
     name: 'Blog',
     col: 'col-span-3 sm:col-span-1',
   },
   {
-    img: 'https://amazonaid.org/wp-content/uploads/2019/02/ezgif.com-optimize.gif',
+    img: 'https://64.media.tumblr.com/4393e9e60edec4a6e821d5f14892a87a/af64a96270ae26f2-ea/s400x600/e8c3dae337ef0dadb718ac4718cac91631265f5b.gifv',
     name: 'Sobre',
     col: 'col-span-5 sm:col-span-1',
   },
   {
-    img: 'https://i.pinimg.com/originals/fa/e4/85/fae4856baa42a511b653a225359bc7ee.gif',
+    img: 'https://64.media.tumblr.com/a72bd03fd505da05c2062d6191a8aa8a/8305528fa1608264-fd/s640x960/959cb2c224904b96b93b4a04a6beedfa810937fd.gif',
     name: 'Dons',
     link:'/painel-dons',
     col: 'col-span-3 sm:col-span-3',
   },
   {
-    img: 'https://media4.giphy.com/media/S9hmjJG3ZL4gCSWXze/giphy.gif?cid=790b7611a6244ac30bd8cab58718cf6f40efb92a3b78e81a&rid=giphy.gif&ct=g',
+    img: 'https://64.media.tumblr.com/bbd00f6149aeeb146b07c9fc5b778407/8305528fa1608264-fc/s640x960/e5dc5fbc6bf1c866c5135161e9962e13984145fe.gif',
     name: 'Perfil',
     col: 'col-span-2 sm:col-span-2',
   },
   {
-    img: 'https://media3.giphy.com/media/eGNpmOUeIIYUB4P4Al/giphy.gif?cid=790b76113d42c68657c63c771efd15378e71009f3ac51c5f&rid=giphy.gif&ct=g',
+    img: 'https://64.media.tumblr.com/46bfcab063ecc267e34e95f163a55870/8305528fa1608264-22/s640x960/4bf52343deb15f79350bd118c6a2d06fd192f045.gif',
     name: 'Rituais',
     col: 'col-span-2 sm:col-span-1',
   },
   {
-    img: 'https://i.pinimg.com/originals/87/81/0a/87810a0cf7c9f3d787b2a4059ab601ed.gif',
+    img: 'https://i.pinimg.com/originals/27/4d/1d/274d1d08a0366d47bd20fe5bd1c447ff.gif',
     name: 'Parceiros',
     col: 'col-span-3 sm:col-span-2',
   },
@@ -80,7 +80,7 @@ export default class Painel extends React.Component {
             to={list.link}
             >
             <img
-              src={require(`${list.img}`)}
+              src={`${list.img}`}
               alt="werewolf"
               className="h-full w-full absolute object-cover"
             />
