720bb343e25ce83368d4d4f7c5202a051cdbcca7
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-20 18:31:40 -0300

    Removendo console.logs da aplicação

--- back-end/src/model/connection.js
+++ back-end/src/model/connection.js
@@ -1,12 +1,21 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
+const MYSQLDATABASE = 'railway';
+// const MYSQLDATABASE = 'guia_das_matilhas';
+const MYSQLHOST = 'containers-us-west-49.railway.app';
+// const MYSQLHOST = 'localhost';
+const MYSQLUSER = 'root';
+const MYSQLPORT = 7558;
+// const MYSQLPORT = 3307;
+const MYSQLPASSWORD = 'kdZI082Y6hibpF5fHaWl';
+// const MYSQLPASSWORD = 'root';
 
 const connection = mysql.createPool({
-  host: process.env.MYSQLHOST || 'localhost',
-  user: process.env.MYSQLUSER || 'root',
-  port: process.env.MYSQLPORT || 3307,
-  password: process.env.MYSQLPASSWORD || 'root',
-  database: process.env.MYSQLDATABASE || 'guia_das_matilhas',
+  host: MYSQLHOST,
+  user: MYSQLUSER,
+  port: MYSQLPORT,
+  password: MYSQLPASSWORD,
+  database: MYSQLDATABASE,
 });
 
 module.exports = connection;
--- front-end/src/fetch.js
+++ front-end/src/fetch.js
@@ -1,9 +1,9 @@
 
-// const REACT_APP_API_HOST = 'guia-das-matilhas-back-end.up.railway.app';
-const REACT_APP_API_HOST = 'localhost:3001';
+const REACT_APP_API_HOST = 'guia-das-matilhas-back-end.up.railway.app';
+// const REACT_APP_API_HOST = 'localhost:3001';
 
-// const REACT_APP_PROTOCOL = 'https';
-const REACT_APP_PROTOCOL = 'http';
+const REACT_APP_PROTOCOL = 'https';
+// const REACT_APP_PROTOCOL = 'http';
 
 export default function fetch() {
   const HOST = REACT_APP_API_HOST;
