fb8860000d988246d07acfa565091d490127888b
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-20 15:54:36 -0300

    Inserção de ghpages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -23,7 +23,7 @@ function App() {
     <AnimateSharedLayout>
       <AnimatePresence>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/guiadasmatilhas" component={Home} />
           <Route path="/trybes" component={Trybes} />
           <Route path="/breeds" component={Breeds} />
           <Route path="/auspices" component={Auspices} />
