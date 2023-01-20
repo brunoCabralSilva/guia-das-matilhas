3acadf3cb5d99bbfb54ad1ac9f0ed82090c79eba
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-20 16:50:43 -0300

    alternando de BrowserRouter para HashRouter

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ProviderRegister from './context/registro/ProviderRegister';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <ProviderRegister>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </ProviderRegister>
-  </BrowserRouter>
+  </HashRouter>
 );
